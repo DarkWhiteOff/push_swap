420607c3504578af79b54d12d8d6220220a9749b
Ziggy Amgar <zamgar@paul-f3Br3s7.clusters.42paris.fr>
2024-09-10 19:05:51 +0200

    push_swap beginning

---
+++ push_swap.h
@@ -0,0 +1,101 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdarg.h>
+
+// Get next line
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char				*get_next_line(int fd);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strcat(char *dest, char *src);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strdup(const char *s);
+char				*ft_substr(char const *str, unsigned int start, size_t len);
+size_t				ft_calc(char const *str, unsigned int start, size_t len);
+
+// Ft printf
+
+int					ft_printf(const char *str, ...);
+int					get_next_arg(va_list *ap, char c);
+int					ft_putchar(char c);
+int					ft_itoa2(int n);
+char				*ft_itoa_ft_printf(int n);
+int					ft_count_ft_printf(int n);
+char				*ft_min_ft_printf(char *str);
+int					ft_putstr(char *str);
+//pas utilisée
+int					ft_dec_into_hex(unsigned int n, int b);
+void				ft_print_array(char *array, int i);
+char				*ft_fill_hexarray(unsigned int n, char *string,
+						char *array);
+int					ft_get_size_hexarray(unsigned int n);
+int					ft_unsigned_itoa2(unsigned int n);
+char				*ft_unsigned_itoa(unsigned int n);
+int					ft_unsigned_count(unsigned int n);
+int					ft_putptr(unsigned long long adress);
+char				*ft_fill_ptrarray(unsigned long long adress,
+						char *string, char *array);
+int					ft_get_size_ptrarray(unsigned long long adress);
+int					ft_putstr(char *str);
+
+// Libft
+
+typedef struct s_list
+{
+	void				*content;
+	struct s_list		*next;
+}	t_list;
+
+int					ft_atoi(const char *str);
+void				ft_bzero(void *str, size_t n);
+void				*ft_calloc(size_t nmemb, size_t size);
+int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isascii(int c);
+int					ft_isdigit(int c);
+int					ft_isprint(int c);
+int					ft_isspace(int c);
+int					ft_islower(int c);
+int					ft_isupper(int c);
+char				*ft_itoa(int n);
+void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memset(void *s, int c, size_t n);
+void				ft_putchar_fd(char c, int fd);
+void				ft_putendl_fd(char *str, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_putstr_fd(char *str, int fd);
+char				**ft_split(char const *s, char c);
+size_t				ft_strlcat(char *dst, const char *src, size_t dsize);
+size_t				ft_strlcpy(char *dst, const char *src, size_t dsize);
+size_t				ft_strlen(const char *s);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+char				*ft_strnstr(const char *str, const char *tofind, size_t n);
+char				*ft_strrchr(const char *s, int c);
+char				*ft_strtrim(char const *s1, char const *set);
+int					ft_tolower(int c);
+int					ft_toupper(int c);
+void				ft_striteri(char *s, void (*f)(unsigned int, char*));
+t_list				*ft_lstnew(void *content);
+int					ft_lstsize(t_list *lst);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
+
+#endif
